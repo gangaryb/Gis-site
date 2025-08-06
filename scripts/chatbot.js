3ba3f868666503662c1355b65cfd3e15e70c173e
gangaryb <gangaryhoudou@gmail.com>
2025-08-05 19:37:48 -0500

    Upload scripts/chatbot.js

---
+++ scripts/chatbot.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bot = document.getElementById("chatbot");
+  if(bot){
+    bot.innerHTML = "<p>🤖 GIS Bot: How can I help you today?</p>";
+  }
+});
